5a05f94ae90d49bc92faeeaa7286ed48e40c26e8
KetanGupta05 <gketan86@gmail.com>
2025-06-04 05:09:25 +0530

    solve bug

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tls: true,                    
+  tlsAllowInvalidCertificates: true
 });
 
 app.use('/api/users', userRoutes);
